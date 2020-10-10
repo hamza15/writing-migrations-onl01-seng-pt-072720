937b794049da00f42a3078d73b8e88a7c89e774d
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-10 19:16:34 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,6 +3,5 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_columns :students, :grade, :integer
     add_columns :students, :birthdate, :string
-    end
   end
 end
